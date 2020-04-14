1ed7e62540b394d59af2159c264bce0333d2b819
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-04-14 02:13:51 +0200

    Lesson 4 - Scenes

---
+++ 4-Scenes/src/init.js
@@ -0,0 +1,23 @@
+const config = {
+    width: 800,
+    height: 600,
+    parent: "container",
+    type: Phaser.CANVAS,
+    backgroundColor: '#392542',
+    scene: [SceneA, SceneB]
+    // scene: [SceneA]
+}
+
+var game = new Phaser.Game(config);
+
+function preload(){
+    
+}
+
+function create(){
+   
+}
+
+function update(time, delta){
+    
+}
